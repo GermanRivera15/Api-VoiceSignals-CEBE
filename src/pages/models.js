e51b1ed9f71aba83749986e85be205b215516687
GermanRivera15 <griveraol@unprg.edu.pe>
2023-02-05 11:44:14 -0500

    Model 3D Update

--- src/pages/models.js
+++ src/pages/models.js
@@ -1,23 +1,23 @@
 // import ContainerModel from "../components/model"
 import React  from "react";
-
+import '../Styles/Model.css'
 export default function Models() {
   let arreglo = [
-    { Code: 0, Name: "https://drive.google.com/file/d/1KMU7eGxkVMbGqCiCJ5zdTDQ8_ZPZtotL/view?usp=share_link" },
-    // { Code: 1, Name: "/models/boom_2.glb" },
-    // { Code: 2, Name: "/models/Canoe.glb" },
-    // { Code: 3, Name: "/models/Canoe.glb" },
-    // { Code: 4, Name: "/models/Canoe.glb" },
-    // { Code: 5, Name: "/models/Canoe.glb" },
-    // { Code: 6, Name: "/models/Canoe.glb" },
-    // { Code: 7, Name: "/models/Canoe.glb" },
-    // { Code: 8, Name: "/models/Canoe.glb" },
-    // { Code: 9, Name: "/models/Canoe.glb" },
-    // { Code: 10, Name: "/models/Canoe.glb" },
-    // { Code: 11, Name: "/models/Canoe.glb" },
-    // { Code: 12, Name: "/models/Canoe.glb" },
-    // { Code: 13, Name: "/models/Canoe.glb" },
-    // { Code: 14, Name: "/models/Canoe.glb" }
+    { Code: 0, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 1, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 2, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 3, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 4, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 5, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 6, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 7, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 8, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 9, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 10, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 11, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 12, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 13, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
+    { Code: 14, Name: "/models/model/bmw.glb", Poster:'/models/poster/avatarTest.JPG'},
   ]
 
   const modelRef = React.useRef();
@@ -29,12 +29,19 @@ export default function Models() {
           arregloMap =>
             <li key= {arregloMap.Code}>
               <model-viewer
+                class='model'
                 src={arregloMap.Name}
-                alt="A rock"
+                poster={arregloMap.Poster}
+                auto-rotate
+                shadow-intensity="1"
                 camera-controls
+                touch-action="pan-y"
+                disable-zoom
+
                 ref={(ref) => {
                   modelRef.current = ref;
                 }}
+
               >
               </model-viewer>
 
