618d8015844d9482ea8e1e15da1fb3b0d1f5bb17
GermanRivera15 <griveraol@unprg.edu.pe>
2023-06-24 14:36:15 -0500

    QR Model App

--- src/pages/QRSign.js
+++ src/pages/QRSign.js
@@ -10,13 +10,13 @@ export default function QRSign() {
         <Card.Header>DICCIONARIO</Card.Header>
         <Card.Body className='card-body-qr-container'>
           <div className='qr-container'>
-            <QRContainer section={process.env.REACT_APP_URL + '/Abecedario'} title={'ABECEDARIO'} />
-            <QRContainer section={process.env.REACT_APP_URL + '/Números'} title={'NÚMEROS'} />
-            <QRContainer section={process.env.REACT_APP_URL + '/'} title={'PALABRAS MÁGICAS'} />
-            <QRContainer section={process.env.REACT_APP_URL + '/Relaciones%20Familiares%20y%20Personales'} title={'FAMILIA'} />
-            <QRContainer section={process.env.REACT_APP_URL + '/Frutas'} title={'FRUTAS'} />
-            <QRContainer section={process.env.REACT_APP_URL + '/Verduras'} title={'VERDURAS'} />
-            <QRContainer section={process.env.REACT_APP_URL + '/Bebidas'} title={'BEBIDAS'} />
+            <QRContainer section={process.env.REACT_APP_URL + '/dictionary/Abecedario'} title={'ABECEDARIO'} />
+            <QRContainer section={process.env.REACT_APP_URL + '/dictionary/Números'} title={'NÚMEROS'} />
+            <QRContainer section={process.env.REACT_APP_URL + '/dictionary/Palabras%20Mágicas'} title={'PALABRAS MÁGICAS'} />
+            <QRContainer section={process.env.REACT_APP_URL + '/dictionary/Relaciones%20Familiares%20y%20Personales'} title={'FAMILIA'} />
+            <QRContainer section={process.env.REACT_APP_URL + '/dictionary/Frutas'} title={'FRUTAS'} />
+            <QRContainer section={process.env.REACT_APP_URL + '/dictionary/Verduras'} title={'VERDURAS'} />
+            <QRContainer section={process.env.REACT_APP_URL + '/dictionary/Bebidas'} title={'BEBIDAS'} />
           </div>
         </Card.Body>
         </ Card>
